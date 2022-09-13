39aa41a690d1c0b30ba5ab272024ef2ee548bab5
Rob Laughlin <waterfallkicks@gmail.com>
2022-09-13 09:30:45 -0700

    8 Submission

---
+++ (8) String to Integer (atoi).py
@@ -0,0 +1,35 @@
+VALID_DIGITS = set(map(lambda d: str(d), range(10)))
+
+def myAtoi(s):
+    digits = '0'
+    sign = '+'
+
+    i = 0
+    while i < len(s) and s[i] == ' ':
+        i += 1
+    
+    if i >= len(s):
+        return 0
+    
+    if s[i] not in VALID_DIGITS:
+        if s[i] == '+' or s[i] == '-':
+            sign = s[i]
+        else:
+            return 0
+        
+        i += 1
+    
+    while i < len(s) and s[i] in VALID_DIGITS:
+        digits += s[i]
+        i += 1
+    
+    maxVal = 2**31
+    n = int(sign + digits)
+    n = min(n, maxVal-1)
+    n = max(n, -maxVal)
+
+    return n
+
+if __name__ == '__main__':
+    s = "4193 with words"
+    print(myAtoi(s))
